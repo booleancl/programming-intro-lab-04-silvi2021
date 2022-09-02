007756ef4bf6ff295b1a2948a16b07c283c53ede
silvia <villalobos.veas@gmail.com>
2022-09-02 11:11:00 -0400

    operaciones basicas string

--- intro_to_python.py
+++ intro_to_python.py
@@ -4,6 +4,21 @@
 print("Hola Mundo")
 print ("Hola Mundo")
 
-
+# La indentación es importante.Es parte de la sintaxis.
+# En este caso la linea 12 debe estar identada para ser parte del if
 if 5 > 2:
     print("cinco es mayor q dos")
+ #cuando lleva comilla es texto y no se ejecuta pa expresión 
+ # Cuando no lleva comillas si se ejecuta la expesión
+
+    print(40+2)
+    print("40+2")
+
+    # Operaciones matematicas
+    print(7+6)
+    print(42/6)
+
+    # Si no pongo las comillas en una palabra,es sintaxis invalida
+    #print(hola mundo)
+
+    print
